03a24347db19d492569989c10eeab053995e6cad
Leear <leear.ramirez@msunaawan.edu.ph>
2025-12-04 18:28:26 +0800

    Video 5 -  Detail Page for Properties- LEEAR RAMIREZ

--- property/models.py
+++ property/models.py
@@ -1,6 +1,5 @@
 import uuid
 
-
 from django.conf import settings
 from django.db import models
 
@@ -11,7 +10,7 @@ from useraccount.models import User
 
 
 class Property(models.Model):
-    id = models.BigAutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.TextField()
     price_per_night = models.IntegerField()
@@ -23,12 +22,9 @@ class Property(models.Model):
     category = models.CharField(max_length=255)
     # favorited = models.ManyToManyField(User, related_name='favorites', blank=True)
     image = models.ImageField(upload_to='uploads/properties')
-    landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE, null=True, blank=True)
+    landlord = models.ForeignKey(User, related_name='properties', on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
 
 
     def image_url(self):
-        return f'{settings.WEBSITE_URL.rstrip("/")}{self.image.url}'
+        return f'{settings.WEBSITE_URL}{self.image.url}'
